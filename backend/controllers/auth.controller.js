7442c2df57257cf5d959052c753deaa127744c8d
Shashank-Naikal <shashanknaikal27@gmail.com>
2024-06-23 21:36:54 +0530

    configured user sign-up function

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -11,21 +11,17 @@ export const signup = async (req, res) => {
 
     const user = await User.findOne({ username });
 
-    if(user){
-        res.status(200).json({message: "UserName already exists"})
+    if (user) {
+      return res.status(200).json({ message: "UserName already exists" });
     }
 
     const salt = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(password, salt);
 
     const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
-	const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
-    const newUser = new User({
-        
-    })
-
-
+    const newUser = new User({});
   } catch (error) {}
 };
 
